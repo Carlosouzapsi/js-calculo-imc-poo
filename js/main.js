aef6e996d31d3401400d4c6487e413fdd9c5c85e
Carlos Otávio de Souza <carlosotavio_souza@apple.com>
2022-11-12 09:17:18 -0300

    atualizando projeto Imc

--- js/main.js
+++ js/main.js
@@ -6,25 +6,64 @@
 
 class Pessoa {
   //Atributos = variáveis ou características
-  nome; // String
-  idade; // Integer
-  peso; // tipo Float ou Real
-  altura; // tipo Float ou Real
+  _nome; //  String esse atributo é protegido por causa do _
+  _idade; // Integer
+  _peso; // tipo Float ou Real
+  _altura; // tipo Float ou Real
+  _imc;
+
   //Métodos = funções ou comportamentos
+
+  // Método construtor
+  constructor(nome, idade, peso, altura) {
+    this._nome = nome;
+    this._idade = idade;
+    this._peso = peso;
+    this._altura = altura;
+    // Caso queira já queira o resultado no objeto pode ser asism:
+    this._imc = this._peso / (this._altura * this._altura);
+  }
+  // ou dá pra criar um Método para calcular o Imc
+  calculaImc() {
+    return (this._peso / (this._altura * this._altura)).toFixed(2);
+  }
+  // get = pegar informação
+  get nome() {
+    return this._nome;
+  }
+  get imc() {
+    return this._imc;
+  }
+
+  get idade() {
+    return this._idade;
+  }
+  // set = configurar, editar, alterar
+  set nome(novoNome) {
+    this._nome = novoNome;
+  }
+  set idade(novaIdade) {
+    this._idade = novaIdade;
+  }
 }
-
 // Criar novo objeto a partir de uma classe
-const pessoa1 = new Pessoa(); // instância de uma pessoa
-const pessoa2 = new Pessoa();
+const pessoa1 = new Pessoa("Edson Maia", 40, 84.5, 1.76); // instância de uma pessoa
+const pessoa2 = new Pessoa("Maria", 66, 73.4, 1.63);
+const pessoa3 = new Pessoa("Ana", 20, 42.3, 1.41);
+const pessoa4 = new Pessoa("Bia", 50, 98.7, 1.52);
 
-// Atribuir valores para os atributos de um objeto
-pessoa1.nome = "Edson";
-pessoa1.idade = 40;
-pessoa1.peso = 84.5;
-
-pessoa2.nome = "Maria";
-pessoa2.idade = 66;
-pessoa2.peso = 63.2;
-
+// Imprimindo somente os objetos
 console.log(pessoa1);
 console.log(pessoa2);
+console.log(pessoa3);
+console.log(pessoa4);
+
+// Imprimindo os objetos só que usando o calc do Imc junto com o metodo
+console.log(pessoa1.calculaImc());
+console.log(pessoa2.calculaImc());
+console.log(pessoa3.calculaImc());
+console.log(pessoa4.calculaImc());
+
+// Usando os métodos getters e setters
+console.log("Nome " + pessoa1.nome);
+
